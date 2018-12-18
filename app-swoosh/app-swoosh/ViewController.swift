c6af4421d647d7df496bce49a88f8efbe469ed46
Andreas Schultz <andreasschultz@Andreass-MBP.fritz.box>
2018-12-18 15:51:54 +0100

    deleted programmatic frames

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -17,9 +17,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
-        
-        bgImage.frame = view.frame
     }
 
 
