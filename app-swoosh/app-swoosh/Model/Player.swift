5e4c5cc7ce588cf2ae04dcb526222bdfacf63203
Andreas Schultz <andreasschultz@Andreass-MBP.fritz.box>
2018-12-18 18:52:44 +0100

    actions and model layer

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Andreas Schultz on 18.12.18.
+//  Copyright © 2018 Andreas Schultz. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String? 
+}
--- app-swoosh/app-swoosh/New Group/LeagueVC.swift
+++ app-swoosh/app-swoosh/New Group/LeagueVC.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var nextButton: BorderedButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        player = Player()
         
+        nextButton.isEnabled = false
     }
     
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+    }
     
     
     @IBAction func nextButtonTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
+    
+    @IBAction func mansButtonTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func womansButtonTapped(_ sender: Any) {
+        selectLeague(leagueType: "womans")
+    }
+    
+    @IBAction func coedButtonTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
     
 }
