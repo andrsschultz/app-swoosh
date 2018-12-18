348c319bff1d3cd08c170fa9549a2f9d9b9fc578
Andreas Schultz <andreasschultz@Andreass-MBP.fritz.box>
2018-12-18 15:00:09 +0100

    elements positioned by programmatic frames

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var swooshLogo: UIImageView!
+    @IBOutlet var bgImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
+        
+        bgImage.frame = view.frame
     }
 
 
